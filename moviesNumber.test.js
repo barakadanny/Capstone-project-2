522a74ca917bb27c65318222f91ed9d4b8c5a2a1
barakadanny <53088629+barakadanny@users.noreply.github.com>
2022-09-01 17:38:49 +0300

    Fix movies count test

--- moviesNumber.test.js
+++ moviesNumber.test.js
@@ -1,17 +1,18 @@
 import movies from './__mocks__/moviesNumber.js';
 
-const array1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-const array2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'];
-const array3 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'o', 'p', 'q', 'r'];
+const moviesArray = [
+  'Trailer',
+  'Welcome to america',
+  'Happy',
+  'Bootcamp',
+  'Africa',
+  'Microverse',
+  'Story',
+  'Coding',
+];
 
 describe('Number of movies test', () => {
   test('Number of movies', () => {
-    expect(movies(array1)).toBe(8);
-  });
-  test('Number of movies', () => {
-    expect(movies(array2)).toBe(13);
-  });
-  test('Number of movies', () => {
-    expect(movies(array3)).toBe(16);
+    expect(movies(moviesArray)).toBe(8);
   });
 });
